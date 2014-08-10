79165e20154d43a35345c467165b02f81fb1726e
Anderson Moreira <anderson.moreira@vtex.com.br>
2014-08-10 14:21:38 -0300

    Remove unnecessary csproj's

--- tool/functions/Get-VersionFromGitTag.ps1
+++ tool/functions/Get-VersionFromGitTag.ps1
@@ -2,8 +2,12 @@ function Get-VersionFromGitTag {
     Write-Host "VersionFromGitTag"
     $tag = Get-GitTagOnLastCommit
 
+    Write-Host "Tag '$tag'"
+
     $tag | Select-String -Pattern "^v(\d+\.\d+\.\d+(-.+)?)$" |
         Foreach-Object { $_.Matches } | Foreach-Object { $_.Groups[1] } | Select-Object -First 1
+    
+    Write-Host "Filtrada '$tag'"
 }
 
 function Get-GitTagOnLastCommit {
